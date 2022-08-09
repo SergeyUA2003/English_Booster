8a9189fbc0824fa561e2559a6d4cb8dd6881ba02
Serhii <meizum2103@gmail.com>
2022-08-10 00:07:19 +0300

    initial commit

---
+++ .vscode/changeButtonCollor.js
@@ -0,0 +1,12 @@
+function onWhiteButtonClick () {
+    console.log ("White Press");
+    document.getElementById("loginDiv").style.color = "white";
+  }
+  function onYellowButtonClick () {
+    console.log ("Yellow Press");
+    document.getElementById("loginDiv").style.color = "yellow";
+  }
+  function onGreenButtonClick () {
+    console.log ("Green Press");
+    document.getElementById("loginDiv").style.color = "rgb(18, 255, 18)";
+  }
